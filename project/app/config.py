4b340206ae0004ac8de7e39bd06fd913497bcfe4
sbazh <sbazhmin@yandex.ru>
2020-09-02 12:57:42 +0300

    first scripts

---
+++ project/app/config.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+
+log = logging.getLogger(__name__)
+
+
+class Settings(BaseSettings):
+    environent: str = os.getenv('ENVIRONMENT', 'dev')
+    testing: bool = os.getenv('TESTING', 0)
+
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info('Loading config from the environment...')
+    return Settings()
+
